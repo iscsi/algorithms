00b8e68cd8ebbb0374b351b73021ec3ac9b02977
iscsi <niscsi@yahoo.com>
2017-03-19 23:36:05 +0100

    Add Kuhn matching algorithm (bipartite graph)

---
+++ matching/kuhn.cpp
@@ -0,0 +1,31 @@
+//Kuhn O(V*E)
+//https://sites.google.com/site/indy256/algo/kuhn_matching2
+
+bool findPath(const vector<vector<int> >& g, int nodeId, vector<int>& matching, vector<bool>& vis)
+{
+	vis[nodeId] = true;
+	for (int BSideId : g[nodeId])
+	{
+		int u = matching[BSideId];
+		if (u == -1 || (!vis[u] && findPath(g, u, matching, vis)))
+		{
+			matching[BSideId] = nodeId;
+			return true;
+		}
+	}
+	return false;
+}
+
+int maxMatching(const vector<vector<int> >& g, int B)
+{
+	int A = g.size();
+	vector<int> matching(B, -1);
+	int res = 0;
+	REP(u, A)
+	{
+		vector<bool> vis(A, false);
+		if (findPath(g, u, matching, vis))
+			++res;
+	}
+	return res;
+}
