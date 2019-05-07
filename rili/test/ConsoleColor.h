47a9b7efdf76c8fabc5e0dee40316558261ec5cc
Istvan Nagy <istvan.nagy@denuvo.com>
2019-05-07 16:29:52 +0200

    colorize test 2

--- rili/test/ConsoleColor.h
+++ rili/test/ConsoleColor.h
@@ -59,7 +59,7 @@ inline std::ostream& green(std::ostream &s)
 	if (isGitlabCi())
 		writeConsole("\x1b[92m");
 	else if (isPowerShell())
-		writeConsole("##vso[task.setvariable result=Succeeded]");
+		writeConsole("##vso[task.complete result=Succeeded;]");
 #endif
 	return s;
 }
