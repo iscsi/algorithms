582e8d9776c1acbd1ed5200dc6a4e8103a520c25
Istvan Nagy <istvan.nagy@denuvo.com>
2020-05-13 16:14:34 +0200

    recursive rename the new template

--- lib/new_template_gen.py
+++ lib/new_template_gen.py
@@ -6,13 +6,22 @@ import subprocess
 from shutil import copyfile
 import string
 
+def replace(folder_path, old, new):
+    for path, subdirs, files in os.walk(folder_path):
+        for name in files:
+            if(old.lower() in name.lower()):
+                file_path = os.path.join(path,name)
+                new_name = os.path.join(path,name.lower().replace(old,new))
+                os.rename(file_path, new_name)
+
 if len(sys.argv) == 2:
-	newProject = int(sys.argv[2])
+	newProject = sys.argv[1]
 	newProjectLower = newProject.lower()
 	newProjectUpper = newProject.upper()
 	dir_util.copy_tree("template",newProjectLower)
 	
 	#rename files template -> newProjectLower
+	replace(newProjectLower, "template", newProjectLower)
 
 	#replace all files
 	#template -> newProjectLower
