f46686bb29fb50c2371dcab919064e8e4d488c1e
Istvan Nagy <istvan.nagy@denuvo.com>
2020-08-11 07:39:00 +0200

    segment tree add fixes + build

--- lib/segment_tree/src/segment_tree_itmo.h
+++ lib/segment_tree/src/segment_tree_itmo.h
@@ -15,32 +15,49 @@ struct SegmentTreeITMO {
 	vector<T> data;
 
 	void init(int n) {
-		size = 1; 
+		size = 1;
 		while (size < n)
 			size *= 2;
 		data.assign(2 * size, T());
 	}
 
+	void build(const vector<T>& a, int x, int lx, int rx) {
+		if (rx == lx + 1) {
+			if (lx < (int)a.size()) {
+				data[x] = a[lx];
+			}
+			return;
+		}
+		int m = (lx + rx) / 2;
+		build(a, 2 * x + 1, lx, m);
+		build(a, 2 * x + 2, m, rx);
+		data[x] = data[2 * x + 1] + data[2 * x + 2];
+	}
+
+	void build(const vector<T>& a) {
+		build(a, 0, 0, size);
+	}
+
 	//index, value, current node, left border, right border
 	void set(int idx, T v, int x, int lx, int rx) {
-		if (rx = lx == 1) {
+		if (rx == lx + 1) {
 			//the bottom level
 			data[x] = v;
 			return;
 		}
 		int m = (lx + rx) / 2;
-		if (i < m) {
+		if (idx < m) {
 			set(idx, v, 2 * x + 1, lx, m);
 		}
 		else {
 			set(idx, v, 2 * x + 2, m, rx);
 		}
 		//the operation + here
-		data[x] = data[2x + 1] + data[2x + 2];
+		data[x] = data[2 * x + 1] + data[2 * x + 2];
 	}
 
 	void set(int idx, T v) {
-		set(0, v, 0, 0, size);
+		set(idx, v, 0, 0, size);
 	}
 	T get(int l, int r, int x, int lx, int rx) {
 		if (lx >= r || rx <= l) return 0;//outside
@@ -50,8 +67,8 @@ struct SegmentTreeITMO {
 		T r2 = get(l, r, 2 * x + 2, m, rx);
 		return r1 + r2;//the operation
 	}
-	
+
 	T get(int l, int r) {
-		get(0, v, 0, 0, size);
+		return get(l, r, 0, 0, size);
 	}
 };
